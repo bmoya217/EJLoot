884c6d0aef51d837b4c7b8fa6d3fc1218a6acb47
Branden Moya <bmoya217@gmail.com>
2026-06-20 18:28:20 -0700

    minimap button dropdown options

--- src/Minimap.lua
+++ src/Minimap.lua
@@ -4,22 +4,78 @@ local function addTooltipLines(tooltip)
     local settings = EJLoot:GetSettings()
     tooltip:SetText("EJ Loot")
     tooltip:AddLine("Left-click: show/hide frame", 1, 1, 1)
-    tooltip:AddLine("Right-click: toggle screen / Adventure Guide position", 1, 1, 1)
+    tooltip:AddLine("Right-click: options", 1, 1, 1)
     tooltip:AddLine("Drag: move minimap button", 1, 1, 1)
     tooltip:AddLine(" ")
     tooltip:AddLine("Display: " .. settings.display, 0.8, 0.8, 0.8)
     tooltip:AddLine("Position: " .. settings.positionMode, 0.8, 0.8, 0.8)
 end
 
-function EJLoot:UpdateMinimapTooltip()
-    if not self.minimapButton or not self.minimapButton:IsMouseOver() then
+function EJLoot:RefreshMinimapTooltip()
+    local tooltip = self.minimapTooltip
+
+    if not tooltip or not tooltip:IsShown() then
         return
     end
 
-    GameTooltip:SetOwner(self.minimapButton, "ANCHOR_LEFT")
-    GameTooltip:ClearLines()
-    addTooltipLines(GameTooltip)
-    GameTooltip:Show()
+    tooltip:ClearLines()
+    addTooltipLines(tooltip)
+    tooltip:Show()
+end
+
+function EJLoot:ToggleMinimapMenu(anchor)
+    local dropdown = self.minimapDropdown
+    local dropdownMenu = L_EasyMenu
+    local dropdownTemplate = "L_UIDropDownMenuTemplate"
+
+    if not dropdownMenu then
+        self:PrintMessage("Dropdown menu library is missing.")
+        return
+    end
+
+    if not dropdown then
+        dropdown = CreateFrame("Frame", "EJLootMinimapDropdown", UIParent, dropdownTemplate)
+        self.minimapDropdown = dropdown
+    end
+
+    local frameDisplay = self:IsFrameShownSetting() and "Hide frame" or "Show frame"
+    local positionDisplay = self:IsFrameAnchoredSetting() and "Move to screen" or "Anchor to Adventure Guide"
+    local menu = {
+        {
+            text = "EJ Loot",
+            isTitle = true,
+            notCheckable = true
+        },
+        {
+            text = frameDisplay,
+            notCheckable = true,
+            func = function()
+                EJLoot:ToggleFrameDisplay()
+            end
+        },
+        {
+            text = positionDisplay,
+            notCheckable = true,
+            func = function()
+                EJLoot:TogglePositionMode()
+            end
+        },
+        {
+            text = "",
+            disabled = true,
+            notCheckable = true
+        },
+        {
+            text = "Hide minimap button",
+            notCheckable = true,
+            func = function()
+                EJLoot:SetMinimapButtonShown(false)
+            end
+        }
+    }
+
+    GameTooltip:Hide()
+    dropdownMenu(menu, dropdown, anchor or self.minimapButton or "cursor", 0, 0, "MENU")
 end
 
 function EJLoot:CreateMinimapButton()
@@ -40,16 +96,16 @@ function EJLoot:CreateMinimapButton()
         type = "launcher",
         text = "EJ Loot",
         icon = "Interface\\Icons\\INV_Misc_Map02",
-        OnClick = function(_, mouseButton)
+        OnClick = function(button, mouseButton)
             if mouseButton == "RightButton" then
-                EJLoot:TogglePositionMode()
+                EJLoot:ToggleMinimapMenu(button)
             else
                 EJLoot:ToggleFrameDisplay()
+                EJLoot:RefreshMinimapTooltip()
             end
-
-            EJLoot:UpdateMinimapTooltip()
         end,
         OnTooltipShow = function(tooltip)
+            EJLoot.minimapTooltip = tooltip
             addTooltipLines(tooltip)
         end
     })
